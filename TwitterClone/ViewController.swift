9857478ec57f43d4caab3c866d6e4f998f1dd60f
Haedong-Jeon <haedongjeon@icloud.com>
2020-09-18 13:45:41 +0900

    Initial Commit

---
+++ TwitterClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwitterClone
+//
+//  Created by 전해동 on 2020/09/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
