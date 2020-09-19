b4a47735cfd292236f9e1324acc91347e1fc49dd
Haedong-Jeon <haedongjeon@icloud.com>
2020-09-19 14:39:50 +0900

    LoginController 이메일 입력 컨테이너, 비밀번호 입력 컨테이너 구현

--- TwitterClone/Controllers/Authentication/LoginController.swift
+++ TwitterClone/Controllers/Authentication/LoginController.swift
@@ -16,6 +16,30 @@ class LoginController: UIViewController {
         imageView.image = #imageLiteral(resourceName: "TwitterLogo")
         return imageView
     }()
+    
+    private lazy var emailContainerView: UIView = {
+        let view: UIView = UIView()
+        view.heightAnchor.constraint(equalToConstant: 50).isActive = true
+
+        let imageView: UIImageView = UIImageView()
+        view.addSubview(imageView)
+        imageView.image = #imageLiteral(resourceName: "ic_mail_outline_white_2x-1")
+        imageView.anchor(left: view.safeAreaLayoutGuide.leftAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, paddingLeft: 8, paddingBottom: 8)
+        imageView.setDimensions(width: 24, height: 24)
+        return view
+    }()
+    
+    private lazy var passwordContainerView: UIView = {
+        let view: UIView = UIView()
+        view.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        let imageView: UIImageView = UIImageView()
+        view.addSubview(imageView)
+        imageView.image = #imageLiteral(resourceName: "ic_lock_outline_white_2x")
+        imageView.anchor(left: view.safeAreaLayoutGuide.leftAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, paddingLeft: 8, paddingBottom: 8)
+        imageView.setDimensions(width: 24, height: 24)
+        return view
+    }()
     //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,5 +55,12 @@ class LoginController: UIViewController {
         view.addSubview(logoImageView)
         logoImageView.centerX(inView: view, topAnchor: view.safeAreaLayoutGuide.topAnchor, paddingTop: 0)
         logoImageView.setDimensions(width: 150, height: 150)
+        
+        let stack: UIStackView = UIStackView(arrangedSubviews: [emailContainerView, passwordContainerView])
+        stack.axis = NSLayoutConstraint.Axis.vertical
+        stack.spacing = 8
+        
+        view.addSubview(stack)
+        stack.anchor(top: logoImageView.bottomAnchor, left: view.safeAreaLayoutGuide.leftAnchor, right: view.safeAreaLayoutGuide.rightAnchor)
     }
 }
